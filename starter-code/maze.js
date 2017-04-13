3456ea7e64b3df30c1f86528e3bf355f9bb1da16
Juampi <juanpabloloza@gmail.com>
2017-04-13 18:51:35 +0200

    create methhods turn left, turn right, is path foward

--- starter-code/maze.js
+++ starter-code/maze.js
@@ -8,28 +8,147 @@ var Maze = function(map, exit) {
   this._exit = {
     row: exit.row,
     col: exit.col
-  }
-}
+  };
+};
 
-Maze.prototype.turnLeft      = function(){
-}
+Maze.prototype.turnLeft= function(){
+  switch (this.min.dir) {
+    case 0:
+      this.miner.dir=3;
+      break;
+    case 1:
+      this.miner.dir=0;
+      break;
+    case 2:
+      this.miner.dir=1;
+      break;
+    case 3:
+      this.miner.dir=2;
+      break;
+  }
+};
 
 Maze.prototype.turnRight     = function(){
-}
+  switch (this.min.dir) {
+    case 0:
+      this.miner.dir=1;
+      break;
+    case 1:
+      this.miner.dir=2;
+      break;
+    case 2:
+      this.miner.dir=3;
+      break;
+    case 3:
+      this.miner.dir=0;
+      break;
+  }
+};
 
 Maze.prototype.isPathForward = function(){
+if(this.miner.dir===0){
+  if (this.miner.row===0){
+    return false;
+  }
+  else if(this._maze[this.miner.col][this.miner.row-1]==F){
+    return false;
+  }
+  else{
+    return true;
+  }
 }
+  if(this.miner.dir===1){
+    if (this.min.col===5){
+      return false;
+    }
+    else if(this._maze[this.miner.col+1][this.miner.row]==F){
+      return false;
+    }
+    else{
+      return true;
+    }
+    }
+    if(this.miner.dir===2){
+      if (this.min.row===5){
+        return false;
+      }
+      else if(this._maze[this.miner.col][this.miner.row+1]==F){
+        return false;
+      }
+      else{
+        return true;
+      }
+      }
+      if(this.miner.dir===3){
+        if (this.min.col===1){
+          return false;
+        }
+        else if(this._maze[this.miner.col-1][this.miner.row]==F){
+          return false;
+        }
+        else{
+          return true;
+        }
+        }
+  };
+
 
 Maze.prototype.isPathLeft    = function(){
-}
+  if(this.miner.dir===0){
+    if (this.miner.row===0){
+      return false;
+    }
+    else if(this._maze[this.miner.col][this.miner.row-1]==F){
+      return false;
+    }
+    else{
+      return true;
+    }
+  }
+    if(this.miner.dir===1){
+      if (this.min.col===5){
+        return false;
+      }
+      else if(this._maze[this.miner.col+1][this.miner.row]==F){
+        return false;
+      }
+      else{
+        return true;
+      }
+      }
+      if(this.miner.dir===2){
+        if (this.min.row===5){
+          return false;
+        }
+        else if(this._maze[this.miner.col][this.miner.row+1]==F){
+          return false;
+        }
+        else{
+          return true;
+        }
+        }
+        if(this.miner.dir===3){
+          if (this.min.col===1){
+            return false;
+          }
+          else if(this._maze[this.miner.col-1][this.miner.row]==F){
+            return false;
+          }
+          else{
+            return true;
+          }
+          }
+
+};
 
 Maze.prototype.isPathRight   = function(){
-}
+};
 
 Maze.prototype.moveForward   = function(){
-}
+
+};
 
 Maze.prototype.notDone       = function(){
-}
+};
 
 module.exports = Maze;
